572653246b3b6918a6d2db9e83009142e9ce8267
Eric Sondergard <flamtap@users.noreply.github.com>
2016-09-02 23:25:29 -0600

    GroceryItem and Promotion domain objects

---
+++ src/GroceryCo/GroceryCo/Model/GroceryItem.cs
@@ -0,0 +1,15 @@
+﻿namespace GroceryCo.Kiosk.Model
+{
+    public class GroceryItem
+    {
+        public GroceryItem(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/GroceryCo/GroceryCo/Model/Promotion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GroceryCo.Kiosk.Model
+{
+    public class Promotion
+    {
+        public Promotion(DiscountType discountType, GroceryItem groceryItem)
+        {
+            DiscountType = discountType;
+            GroceryItem = groceryItem;
+        }
+
+        public DiscountType DiscountType { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public int DaysInEffect { get; set; }
+
+        public GroceryItem GroceryItem { get; set; }
+    }
+
+    public enum DiscountType
+    {
+        OnSale,
+        Group,
+        AdditionalProduct
+    }
+}
