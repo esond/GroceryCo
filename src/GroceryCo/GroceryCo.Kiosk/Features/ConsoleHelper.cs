6ced5b7b8237e181d8f271c3465c6d97219c7227
Eric Sondergard <flamtap@users.noreply.github.com>
2016-09-05 15:08:34 -0600

    Throw exception if trying to create a menu with an empty collection

--- src/GroceryCo/GroceryCo.Kiosk/Features/ConsoleHelper.cs
+++ src/GroceryCo/GroceryCo.Kiosk/Features/ConsoleHelper.cs
@@ -6,6 +6,9 @@ namespace GroceryCo.Kiosk.Features
     {
         public static int SelectFromStringArray(string[] items)
         {
+            if (items.Length == 0)
+                throw new ArgumentException("items cannot be emtpty", nameof(items));
+
             for (int i = 1; i <= items.Length; i++)
             {
                 Console.WriteLine($"\t[{i}] {items[i - 1]}");
