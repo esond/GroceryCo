79d9ae24e021692a4618ff7798b4bfe71a7b0173
Eric Sondergard <flamtap@users.noreply.github.com>
2016-09-05 20:35:34 -0600

    Adding "Discounted" column to reciept

--- src/GroceryCo/GroceryCo.Kiosk/Features/Cashier/ReceiptGenerator.cs
+++ src/GroceryCo/GroceryCo.Kiosk/Features/Cashier/ReceiptGenerator.cs
@@ -12,17 +12,19 @@ namespace GroceryCo.Kiosk.Features.Cashier
         {
             StringBuilder builder = new StringBuilder();
 
-            List<Tuple<string, string, string>> itemStrings = new List<Tuple<string, string, string>>();
+            List<Tuple<string, string, string, string>> itemStrings = new List<Tuple<string, string, string, string>>();
 
             foreach (PurchaseItem item in purchase.PurchaseItems.OrderBy(i => i.GroceryItemName))
             {
-                itemStrings.Add(new Tuple<string, string, string>(item.GroceryItemName,
-                    item.GroceryItem.Price.ToString("c"), item.DiscountedPrice.ToString("c")));
+                string isDiscounted = item.DiscountedPrice != item.GroceryItem.Price ? "\u2713" : string.Empty;
+
+                itemStrings.Add(new Tuple<string, string, string, string>(item.GroceryItemName,
+                    item.GroceryItem.Price.ToString("c"), item.DiscountedPrice.ToString("c"), isDiscounted));
             }
 
-            builder.Append(itemStrings.ToStringTable(
-                new[] {"Item Name", "Price", "Sale Price"},
-                i => i.Item1, i => i.Item2, i => i.Item3));
+            builder.AppendLine(itemStrings.ToStringTable(
+                new[] {"Item Name", "Price", "Sale Price", "Discounted"},
+                i => i.Item1, i => i.Item2, i => i.Item3, i => i.Item4));
 
             builder.Append($"TOTAL: {purchase.Total:c}");
 
